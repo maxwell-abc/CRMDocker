374bcf1de076d6d87f181546ab5a4f4259314d82
maxwell <maxwell_a@qq.com>
2021-07-15 09:21:11 +0800

    fix

--- src/main/java/com/ec/crm/config/CustomerCasClientConfigurer.java
+++ src/main/java/com/ec/crm/config/CustomerCasClientConfigurer.java
@@ -17,6 +17,6 @@ public class CustomerCasClientConfigurer implements CasClientConfigurer {
     @Override
     public void configureAuthenticationFilter(FilterRegistrationBean authenticationFilter) {
         log.info(this.getClass().getName());
-        authenticationFilter.getInitParameters().put("ignorePattern", "/index|logout");
+        authenticationFilter.getInitParameters().put("ignorePattern", "/index|logout|/*");
     }
 }
